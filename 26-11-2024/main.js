669b4ed0f810d24cba5bf67d61d5e3f7f2768ba0
thevayani <ithevayani1601@gmail.com>
2024-11-27 10:35:55 +0530

    Resume format

---
+++ 26-11-2024/main.js
@@ -0,0 +1,106 @@
+let resume=
+       {
+  name:"thevayani.i",
+  email:"ithevayani1601@gmail.com",
+  phone:"+919443079115",
+  objective:"we’ve put together a selection of powerful IT resume objective statements to help you determine the best way to frame what you have to offer",
+  
+  education_details:[
+    {
+      course_name:"sslc",
+      institute_name:"st_joseph's_convent_Hr_sec_school",
+      year_of_passing:2013,
+      percentage:90,
+    },
+    
+    {
+      course_name:"hsc",
+      institute_name:"st_joseph's_convent_Hr_sec_school",
+      year_of_passing:2015,
+      percentage:80, 
+    },
+    
+    {
+      course_name:"bsc",
+      institute_name:"women's_chiristian_college",
+      year_of_passing:2018,
+      percentage:85.65,
+    },
+      
+      {
+      course_name:"bed",
+      institute_name:"st_hindu_college_of_education",
+      year_of_passing:2020,
+      percentage:90, 
+    },
+    
+    {
+      course_name:"msc",
+      institute_name:"annamalai_university",
+      year_of_passing:2022,
+      percentage:79
+    }
+    ],
+    
+    project:
+         [
+         
+      { 
+        title:"abstract_algebra",
+        company_name:"ckcompany",
+        year:2020
+    }
+        
+         ],
+         
+         
+    work_experience:
+    {
+        company_name:"akcompany",
+        position:"software_developer",
+        duration:2020_2022,
+        year:2020
+
+    },
+
+    certifications:[
+        {
+            certification_name:"full stack web developer",
+            institute_name:"agaram academy",
+            year:2024,
+
+        } 
+    ],
+  
+  personal_details:{
+  father_name:"iyyappan",
+  mother_name:"sivakami",
+  dob:"16-01-1998",
+  maritel_status:"married",
+  language_known:["tamil","english"],
+  skills:["ms_office","office_automation"],
+  nationality:"indian",
+  address:"150_ganeshapuram_road,nagercoil"
+  },
+         
+         
+  declaration:
+         "i solemnly conform all the information provided above is true to the best of my knowledge and ability",
+         
+         
+         
+   date:"25-11-24",
+   place:"nagercoil"
+         
+  }
+
+  console.log(resume.name)
+  console.log(resume.education_details[1].year_of_passing)
+  console.log(resume)
+    
+    
+    
+    
+    
+    
+  
