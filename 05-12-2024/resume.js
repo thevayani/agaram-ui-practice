c5e8f9625d4b0a977665dd35fda335f145b98a4a
thevayani <ithevayani1601@gmail.com>
2024-12-05 16:56:54 +0530

    resume_builder

---
+++ 05-12-2024/resume.js
@@ -0,0 +1,27 @@
+let resume={
+personal_details:{},
+skills:[],
+hobbies:[]
+}
+
+function addValue(e,key,p_key){
+    if(p_key){
+    resume[p_key][key] =e.value;
+    }
+    else{
+    resume[key]=e.value;
+    }
+    display_output();
+
+}
+
+function addArrayValue(key){
+    let value = document.getElementById(key).value;
+    resume[key].push(value)
+    document.getElementById(key).value = "";
+    display_output();
+  }
+
+function display_output(){
+    document.getElementById('output').innerHTML= JSON.stringify(resume);
+}
